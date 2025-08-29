41f4374f8a395d7169e4b6834e826c1d90176560
Artem Ostrovskiy <artem@softwareworksforyou.co.uk>
2025-08-30 03:31:55 +0400

    Initial commit: virglrenderer-venus & qemu-venus formulas

---
+++ Formula/qemu-venus.rb
@@ -0,0 +1,38 @@
+class QemuVenus < Formula
+  desc "QEMU with virglrenderer/OpenGL and Venus (Vulkan over VirtIO)"
+  homepage "https://www.qemu.org/"
+  license "GPL-2.0-or-later"
+  head "https://gitlab.com/qemu-project/qemu.git", branch: "master"
+
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "pixman"
+  depends_on "libepoxy"
+  depends_on "gtk+3"
+  depends_on "libslirp"
+  depends_on "virglrenderer-venus"
+  depends_on "alsa-lib"
+  depends_on "libpng"
+  depends_on "zstd"
+
+  def install
+    args = %W[
+      --prefix=\#{prefix}
+      --enable-gtk
+      --enable-opengl
+      --enable-virglrenderer
+      --enable-slirp
+      --disable-werror
+    ]
+
+    system "./configure", *args
+    system "make", "-j", ENV.make_jobs.to_s
+    system "make", "install"
+
+    ohai "Built QEMU with virglrenderer and Venus support."
+  end
+
+  test do
+    system "\#{bin}/qemu-system-x86_64", "--version"
+  end
+end
---
+++ Formula/virglrenderer-venus.rb
@@ -0,0 +1,28 @@
+class VirglrendererVenus < Formula
+  desc "VirGL virtual 3D GPU driver (with Venus Vulkan support)"
+  homepage "https://gitlab.freedesktop.org/virgl/virglrenderer"
+  license "MIT"
+  head "https://gitlab.freedesktop.org/virgl/virglrenderer.git", branch: "main"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libepoxy"
+  depends_on "libdrm"
+  depends_on "mesa" # for GBM/EGL headers
+  depends_on "wayland"
+  depends_on "xorgproto"
+
+  def install
+    system "meson", "setup", "build",
+           *std_meson_args,
+           "-Dvenus=true",
+           "-Dexamples=false"
+    system "meson", "compile", "-C", "build", "-v"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    system "pkg-config", "--exists", "virglrenderer"
+  end
+end
