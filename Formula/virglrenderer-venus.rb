379b742016ce3481bece19491205b61847a15ff2
Artem Ostrovskiy <artem@softwareworksforyou.co.uk>
2025-08-30 03:49:58 +0400

    Initial commit: virglrenderer-venus & qemu-venus formulas

--- Formula/virglrenderer-venus.rb
+++ Formula/virglrenderer-venus.rb
@@ -7,13 +7,19 @@ class VirglrendererVenus < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "python@3.12" => :build
+  depends_on "pyyaml" => :build
+
   depends_on "libepoxy"
   depends_on "libdrm"
-  depends_on "mesa" # for GBM/EGL headers
+  depends_on "mesa"      # GBM/EGL headers
   depends_on "wayland"
   depends_on "xorgproto"
 
   def install
+    py = Formula["python@3.12"].opt_bin/"python3"
+    ENV["PYTHON"] = py # чтобы meson использовал именно этот python (с установленным pyyaml)
+
     system "meson", "setup", "build",
            *std_meson_args,
            "-Dvenus=true"
@@ -25,3 +31,4 @@ class VirglrendererVenus < Formula
     system "pkg-config", "--exists", "virglrenderer"
   end
 end
+
