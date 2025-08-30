be2156f5f5ce1363b3f5a667723a7ee24c6732ab
Artem Ostrovskiy <artem@softwareworksforyou.co.uk>
2025-08-30 04:47:15 +0400

    direct pyyaml vendor

--- Formula/virglrenderer-venus.rb
+++ Formula/virglrenderer-venus.rb
@@ -14,12 +14,31 @@ class VirglrendererVenus < Formula
   depends_on "mesa"
   depends_on "wayland"
   depends_on "xorgproto"
-  depends_on "libyaml"
+
+  # Берём исходники PyYAML (без колёс, без сети, без C-расширения)
+  resource "pyyaml" do
+    url "https://github.com/yaml/pyyaml/archive/refs/tags/6.0.2.tar.gz"
+    sha256 "9377c381ac3fccad8df73d96b5139ef8b1a2c57a0d913e95ab0a2275d66b5caa"
+  end
 
   def install
+    py = Formula["python@3.12"].opt_bin/"python3"
+    ENV["PYTHON"] = py
+
+    # 1) Разворачиваем PyYAML и КОПИРУЕМ чисто питоновский пакет 'yaml/'
+    vendored = buildpath/"pydeps/site-packages"
+    vendored.mkpath
+    resource("pyyaml").stage do
+      # В архивах PyYAML исходники пакета лежат в папке 'lib/yaml'
+      cp_r Dir["lib/yaml"], vendored
+    end
+    ENV.prepend_path "PYTHONPATH", vendored
+
+    # 2) Обычная сборка virglrenderer с Venus
     system "meson", "setup", "build",
            *std_meson_args,
-           "-Dvenus=true"
+           "-Dvenus=true",
+           "-Dexamples=false"
     system "meson", "compile", "-C", "build", "-v"
     system "meson", "install", "-C", "build"
   end
